82a0a3d35997829e7095a48bc2424a52ece256aa
Yoonho Hann <83288181+hnnynh@users.noreply.github.com>
2024-06-20 16:03:32 +0900

    fix: unify data key
    데이터 일관성을 위한 데이터 key 수정

--- stats-for-host.js
+++ stats-for-host.js
@@ -1,68 +1,74 @@
-"use strict";
+"use strict"
 
 const mongoose = require("mongoose");
 const MONGO_URI = process.env.MONGO_URI;
 const User = require("./models/user");
 
 const arrName = ["animal", "emoji", "color", "first", "now"];
-
+  
 let connection = null;
 
 const connectDB = () => {
   if (connection && mongoose.connection.readyState === 1) return;
-  mongoose.connect(MONGO_URI, {}).then((conn) => {
-    connection = conn;
-  });
+  mongoose.connect(MONGO_URI, { }).then(
+    conn => {
+      connection = conn;
+    }
+  );
 };
 
 module.exports.handler = async (event, context) => {
+
   const hostId = event.pathParameters.hostId;
   const userId = event.requestContext.authorizer.lambda.userId;
-
-  if (hostId != userId)
-    return context.done(null, { status: 403, message: "User Not Allowed" });
-
+  
+  if(hostId != userId)
+    return context.done(null, { "status" : 403, "message" : "User Not Allowed" });
+  
   connectDB();
-
-  try {
+  
+  try{
     const hostUser = await User.findOne({ id: hostId }).populate("friends");
     const total = hostUser.animal.reduce((sum, val) => sum + val, 0);
 
     if (total === 0) {
-      return context.done(null, {
-        status: 204,
-        message: "No Result Yet",
-        data: {
-          image: "000",
-        },
+      return context.done(null, { 
+        "status" : 204, 
+        "message" : "No Result Yet", 
+        "data":{
+          "image" : "000"
+        }
       });
-    }
-
-    let result = {};
+	  }
+	  
+	  let result = {};
     arrName.forEach((name, idx) => {
       const arr = hostUser[name];
       const first = Math.max(...arr);
       const firstIdx = arr.indexOf(first);
       const firstObj = {
-        [name]: firstIdx,
+        index: firstIdx,
         percent: Math.floor((first / total) * 100),
       };
 
       arr[firstIdx] = 0;
       const second = Math.max(...arr);
       const secondObj = {
-        [name]: arr.indexOf(second),
+        index: arr.indexOf(second),
         percent: Math.floor((second / total) * 100),
       };
       result[name] = [firstObj, secondObj];
     });
 
     return context.done(null, {
-      status: 200,
-      message: "Stats Result",
-      data: result,
+      "status": 200,
+      "message": "Stats Result",
+      "data" : result
     });
-  } catch (err) {
+	  
+	  
+  } catch(err){
     return context.failed(err);
   }
+  
 };
